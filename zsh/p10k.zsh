b90dac805e9152536f2dd84e7ff0f3d8154f3d0f
trondvh <trond.hapnes@gmail.com>
2022-07-26 09:50:28 +0200

    Remove leading wildcard on prod kubecontext name

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -1231,7 +1231,7 @@
   #   typeset -g POWERLEVEL9K_KUBECONTEXT_TEST_VISUAL_IDENTIFIER_EXPANSION='⭐'
   #   typeset -g POWERLEVEL9K_KUBECONTEXT_TEST_CONTENT_EXPANSION='> ${P9K_CONTENT} <'
   typeset -g POWERLEVEL9K_KUBECONTEXT_CLASSES=(
-      '*prod*'  PROD    # These values are examples that are unlikely
+      'prod*'  PROD    # These values are examples that are unlikely
       '*test*'  TEST    # to match your needs. Customize them as needed.
       '*'       DEFAULT)
   typeset -g POWERLEVEL9K_KUBECONTEXT_DEFAULT_FOREGROUND=134
